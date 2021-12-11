b11921c854280d88d7728e12d72079a7354418c3
eljosue27 <64979123+eljosue27@users.noreply.github.com>
2021-12-11 13:56:06 -0500

    Second commit

---
+++ main.js
@@ -0,0 +1,61 @@
+let operand1 = null,
+  operation = null,
+  currentInput = "";
+const screen = document.getElementById("result");
+
+function handleNumber() {
+  currentInput += this.value;
+  screen.value = currentInput;
+}
+
+function handleDecimal() {
+  currentInput += ".";
+  screen.value = currentInput;
+}
+function handleOperation() {
+  operation = this.value;
+  operand1 = currentInput;
+  currentInput = "";
+  screen.value = 0;
+}
+
+function handleEquals() {
+  screen.value = calculate(operand1, operation, currentInput);
+  currentInput = " ";
+  operation = null;
+  operand1 = null;
+}
+
+function calculate(o1, op, o2) {
+  switch (op) {
+    case "+":
+      return parseFloat(o1) + parseFloat(o2);
+    case "-":
+      return parseFloat(o1) - parseFloat(o2);
+    case "*":
+      return parseFloat(o1) * parseFloat(o2);
+    case "/":
+      return parseFloat(o1) / parseFloat(o2);
+  }
+}
+
+function clear() {
+  operand1 = null;
+  operation = null;
+  currentInput = "";
+  screen.value = 0;
+}
+
+const numbers = document.getElementsByClassName("number");
+const operations = document.getElementsByClassName("operator");
+
+for (let i = 0; i < numbers.length; ++i) {
+  numbers[i].addEventListener("click", handleNumber, false);
+}
+for (let i = 0; i < operations.length; i++) {
+  operations[i].addEventListener("click", handleOperation, false);
+}
+
+document.getElementById("equals").addEventListener("click", handleEquals);
+document.getElementById("decimal").addEventListener("click", handleDecimal);
+document.getElementById("clear").addEventListener("click", clear);
